a1715e7bdfa3bc20388bef945c5d3824745296de
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2025-09-09 17:06:31 -0500

    Mejora de la landing page

--- src/featured/App.jsx
+++ src/featured/App.jsx
@@ -2,7 +2,12 @@ export default function App () {
 
     return (
 
-        <h1 className="">Dashboard</h1>
+        <div style={{width: '100%', height: '100dvh', display: 'grid', placeItems: 'center'}}>
+            <div>
+                <h1 className="">Dashboard</h1>
+                <p>Este <b>Dashboard</b> está en construcción.</p>
+            </div>
+        </div>
 
     )
 

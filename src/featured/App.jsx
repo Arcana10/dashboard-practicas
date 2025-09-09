df813b2d5ac2cca8e14fd39a469359964f22f625
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2025-09-09 17:39:54 -0500

    Actualización de la landing page

--- src/featured/App.jsx
+++ src/featured/App.jsx
@@ -3,9 +3,11 @@ export default function App () {
     return (
 
         <div style={{width: '100%', height: '100dvh', display: 'grid', placeItems: 'center'}}>
-            <div>
-                <h1 className="">Dashboard</h1>
+            <div style={{textAlign: 'center'}}>
+                <h1>Dashboard</h1>
                 <p>Este <b>Dashboard</b> está en construcción.</p>
+                <br/>
+                <a href="/" style={{display: 'inline-flex', background: '#181818', color: '#fff', padding: '.8rem 1.2rem', borderRadius: '5px'}}>Volver a cargar</a>
             </div>
         </div>
 

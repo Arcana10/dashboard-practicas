12762d1065de94192d9c4abf5b479b0a38183882
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2025-09-09 16:34:38 -0500

    upload project

---
+++ src/featured/App.jsx
@@ -0,0 +1,9 @@
+export default function App () {
+
+    return (
+
+        <h1 className="">Dashboard</h1>
+
+    )
+
+}
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './featured/App'
+
+import './shared/css/global.css'
+
+createRoot(document.getElementById('root')).render(
+
+    <>
+    
+        <StrictMode>
+
+            <App/>
+
+        </StrictMode>
+
+    </>
+
+)
